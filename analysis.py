ecbcf9a6e6dc593d3c882a4d4b234978ad479d2a
Matthew Turner <maturner01@gmail.com>
2017-03-09 15:03:50 -0800

    analysis for counts and agency by phase

--- analysis.py
+++ analysis.py
@@ -35,13 +35,14 @@ COLUMNS_IN_ORDER = [
     'slug',
 ]
 
-sns.set(style="white", context="talk")
-sns.set_palette(sns.color_palette("Set3", 10))
-
 
 def get_analyzer(year):
     '''
-
+    Convenience method for creating a newly initialized instance of the
+    Analyzer class. Currently the only argument is year since the projects all
+    contain a year. In the future we may want to match some other unique
+    element of a title, or create some other kind of wrapper to search
+    all Project names in the metacorps database.
     '''
     if type(year) is int:
         year = str(year)
@@ -74,7 +75,9 @@ class Analyzer:
             ret_dict.update(
                 {
                     rng_name:
-                    _select_range_and_pivot(date_range, counts_df, data_method)
+                    _select_range_and_pivot_daily_counts(
+                        date_range, counts_df, data_method
+                    )
                 }
             )
 
@@ -97,8 +100,117 @@ class Analyzer:
 
         plt.tight_layout(w_pad=0.5, h_pad=-0.5)
 
-    def agency_counts(df):
+    def plot_agency_counts_by_dateranges(self,
+                                         subj_obj,
+                                         date_ranges=DEFAULT_DATE_RANGES,
+                                         pdf_output=False):
+
+        ag = self.agency_counts_by_dateranges(subj_obj)
+
+        if pdf_output:
+            pass
+        else:
+            _plot_agency_counts_by_dateranges(ag, subj_obj, date_ranges)
+
+        plt.tight_layout()
+
+    def network_daily_timeseries(self):
+        """
+        Create a timeseries for every network with each column a facet.
+
+        Arguments: None
+
+        Returns (dict): Dictionary of timeseries as explained in description
+        """
         pass
+
+    def agency_counts_by_dateranges(self,
+                                    subj_obj,
+                                    date_ranges=DEFAULT_DATE_RANGES):
+        """
+        Arguments:
+            subj_obj (str): either 'subjects' or 'objects'
+
+        Returns (dict): dictionary with keys of date ranges as in the
+            per-network facet counts. Values are dataframes with
+            indexes networks and columns with clinton-object, clinton-subject,
+            trump-object, trump-subject. Of course this needs to be generalized
+            for 2012, but leave that as a TODO
+        """
+        counts_df = _count_daily_subj_obj(self.df, subj_obj)
+
+        ret_dict = {}
+        for rng_name, date_range in date_ranges.items():
+            ret_dict.update(
+                {
+                    rng_name:
+                    _select_range_and_pivot_subj_obj(
+                        date_range, counts_df, subj_obj
+                    )
+                }
+            )
+
+        return ret_dict
+
+
+def _select_range_and_pivot_subj_obj(date_range, counts_df, subj_obj):
+
+    rng_sub = counts_df[
+        date_range[0] <= counts_df.start_localtime
+    ][
+        counts_df.start_localtime <= date_range[1]
+    ]
+
+    rng_sub_sum = rng_sub.groupby(['network', subj_obj]).agg(sum)
+
+    ret = rng_sub_sum.reset_index().pivot(
+        index='network', columns=subj_obj, values='counts'
+    )
+
+    return ret
+
+
+def _count_daily_subj_obj(df, sub_obj):
+
+    subs = df[['start_localtime', 'network', 'subjects', 'objects']]
+
+    subs.subjects = subs.subjects.map(lambda s: s.strip().lower())
+    subs.objects = subs.objects.map(lambda s: s.strip().lower())
+
+    try:
+        trcl = subs[
+            (subs[sub_obj].str.contains('hillary clinton') |
+             subs[sub_obj].str.contains('donald trump')) &
+            subs[sub_obj].str.contains('/').map(lambda b: not b) &
+            subs[sub_obj].str.contains('campaign').map(lambda b: not b)
+        ]
+    except KeyError:
+        raise RuntimeError('sub_obj must be "subjects" or "objects"')
+
+    c = trcl.groupby(['start_localtime', 'network', sub_obj]).size()
+
+    ret_df = c.to_frame()
+    ret_df.columns = ['counts']
+    ret_df.reset_index(inplace=True)
+
+    # cleanup anything like 'republican nominee'
+    ret_df.loc[
+        :, sub_obj
+    ][
+
+        ret_df[sub_obj].str.contains('donald trump')
+
+    ] = 'donald trump'
+
+    ret_df.loc[
+        :, sub_obj
+    ][
+
+        ret_df[sub_obj].str.contains('hillary clinton')
+
+    ] = 'hillary clinton'
+
+    return ret_df
 
 
 def _standardize_columns(df_dict):
@@ -119,6 +231,49 @@ def _standardize_columns(df_dict):
 
 def _pdf_plot_facets_by_dateranges(nfc, date_ranges, data_method):
     pass
+
+
+def _plot_agency_counts_by_dateranges(ag, subj_obj, date_ranges):
+
+        sns.set(style="white", context="talk")
+        sns.set_palette(sns.color_palette(["#e74c3c", "#34495e"]))
+
+        max_y = 0.0
+        for df in ag.values():
+            max_y = max(max_y, df.max().max())
+
+        print(max_y)
+
+        fig, axes = plt.subplots(
+            figsize=(7, 9.5), ncols=1, nrows=len(list(ag.keys()))
+        )
+
+        for i, k in enumerate(date_ranges.keys()):
+
+            ag[k].plot(kind='bar', ax=axes[i], rot=0., legend=False)
+            axes[i].set_title(k)
+            axes[i].set_ylim([0.0, max_y + 5])
+
+            axes[i].set_xlabel('')
+            axes[i].yaxis.grid()
+
+        legend = axes[0].legend(
+                loc='upper center', borderaxespad=0.,
+                ncol=5, prop={'size': 12},
+                frameon=True
+        )
+
+        sns.despine(left=True)
+
+        legend.get_frame().set_facecolor('0.95')
+        legend.get_frame().set_linewidth(1.5)
+
+        if subj_obj == 'objects':
+            title = 'Object of metaphorical violence'
+        elif subj_obj == 'subjects':
+            title = 'Subject of metaphorical violence'
+
+        fig.suptitle(title, fontsize=22)
 
 
 def _plot_facets_by_dateranges(nfc, date_ranges, data_method):
@@ -171,7 +326,7 @@ def _count_daily_instances(df):
     return ret_df
 
 
-def _select_range_and_pivot(date_range, df, data_method):
+def _select_range_and_pivot_daily_counts(date_range, df, data_method):
 
     # subset only dates in given date range; earlier date assumed first
     rng_sub = df[
@@ -185,6 +340,9 @@ def _select_range_and_pivot(date_range, df, data_method):
     rng_sub_sum = rng_sub.groupby(['network', 'facet_word']).agg(sum)
 
     # create pivot table so we can barplot color coded by facet word
+    # ret = rng_sub_sum.reset_index().pivot(
+    #     index='network', columns='facet_word', values='counts'
+    # )
     ret = rng_sub_sum.reset_index().pivot(
         index='network', columns='facet_word', values='counts'
     )
